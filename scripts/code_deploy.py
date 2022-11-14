9d3e57e7b8b5b07c87882bf517aff1de2590fe61
udhayakumarpanneerselvam <117716731+udhayakumarpanneerselvam@users.noreply.github.com>
2022-11-14 19:05:01 +0530

    Update code_deploy.py

--- scripts/code_deploy.py
+++ scripts/code_deploy.py
@@ -111,7 +111,7 @@ def sessLogout(infa_session_id):
 print ("Welcome to CI_CD Application")
 
 #Get the Tag Info that has to be used
-iicsTag = input("Enter the IICS Asset Tag : ")
+iicsTag = input("Testdemo")
 
 #Make a Login Call
 
